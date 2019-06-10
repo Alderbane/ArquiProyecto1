f528399a24aabbf054524f273c7cf2df8efa6a3d
Alderbane <alderban@outlook.com>
2019-06-10 10:41:13 -0500

    Skeleton
    Added basic skeleton detailing the recursion

--- Proyecto.asm
+++ Proyecto.asm
@@ -1,6 +1,5 @@
 .data
 
-
 .text
 #numero de torres
 addi $s0, $s0, 3
@@ -17,36 +16,53 @@ addi $a3, $zero, 0x1001
 sll $a3, $a3, 16
 addi $a3, $a3, 0x0040
 
+add $t1, $a1, $zero
 #fill tower 1
 add $t0, $s0, $zero #counter
 loop_fill:
-sw $t0, 0($a1) #add disc
-addi $a1, $a1, 4 #next disc memory value
+sw $t0, 0($t1) #add disc
+addi $t1, $t1, 4 #next disc memory value
 addi $t0, $t0, -1 #next disc value
 bne $t0, $zero, loop_fill
 
-addi $a1, $a1, -4 #make sure the tower pointer points to the last disc
 
-add $t0, $s0, $zero
+
+add $t0, $s0, $zero 
 add $t1, $a1, $zero
 add $t2, $a2, $zero
 add $t3, $a3, $zero
-jal Hanoy
+jal Hanoi
 j end
 
-#algorithm to solve hanoy
-#params: $t0= tower size, $t1 = origen, $t2 = auxiliar, $t3 = destino
-Hanoy:
-addi $sp, $sp, -4
+#algorithm to solve hanoi
+#Hanoi(N, A, B, C)
+#params: $t0= tower size, $t1 = origin, $t2 = auxiliary, $t3 = destiny
+Hanoi:
+addi $sp, $sp, -4	#push return address
 sw $ra, 0($sp)
-bne $t0, 1, last_disc
+bne $t0, 1, move_disc
 
+#Hanoi(N-1, A, C, B)
 
+jal Hanoi
+
+#Hanoi(1, A, B, C)
+
+jal Hanoi
+
+#Hanoi(N-1, B, A, C)
+
+jal Hanoi
+j finish
 move_disc:
 
+lw $t9, 0($t1) #save value of origin tower in a placeholder
+sw $zero, 0($t1) #clean tower value
+addi $t1, $t1, -4 #reduce tower pointer
+sw $t9, 0($t3) #store value in destiny tower
 
-
-lw $ra, 0($sp)
+finish:
+lw $ra, 0($sp)		#pop return address
 addi $sp, $sp, 4
 jr $ra
 
