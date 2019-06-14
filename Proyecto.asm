8c5e42fafa8ad05fb6d0c60dc326a92f88d5b7d0
stompercito <51408323+stompercito@users.noreply.github.com>
2019-06-13 23:35:50 -0500

    Stack for N
    It was added N to the stack and the code to subtract 1 to N

--- Proyecto.asm
+++ Proyecto.asm
@@ -17,6 +17,7 @@ sll $a3, $a3, 16
 addi $a3, $a3, 0x0040
 
 add $t1, $a1, $zero
+
 #fill tower 1
 add $t0, $s0, $zero #counter
 loop_fill:
@@ -24,8 +25,6 @@ sw $t0, 0($t1) #add disc
 addi $t1, $t1, 4 #next disc memory value
 addi $t0, $t0, -1 #next disc value
 bne $t0, $zero, loop_fill
-
-
 
 add $t0, $s0, $zero 
 add $t1, $a1, $zero
@@ -38,14 +37,20 @@ j end
 #Hanoi(N, A, B, C)
 #params: $t0= tower size, $t1 = origin, $t2 = auxiliary, $t3 = destiny
 Hanoi:
-addi $sp, $sp, -4	#push return address
-sw $ra, 0($sp)
+#STACK
+addi $sp, $sp, -8	#push return address
+sw $ra, 0($sp)		#push return N value
+sw $t0, 4($sp)
+
+
 beq $t0, 1, move_disc
 
 #Hanoi(N-1, A, C, B)
 add $t4, $zero, $t2
 add $t2, $zero, $t3
 add $t3, $zero, $t4
+
+sub $t0, $t0, 1 #N-1
 jal Hanoi
 
 #Hanoi(1, A, B, C)
@@ -58,6 +63,7 @@ jal Hanoi
 add $t4, $zero, $t2
 add $t2, $zero, $t1
 add $t1, $zero, $t4
+
 jal Hanoi
 j finish
 move_disc:
@@ -69,8 +75,9 @@ sw $t9, 0($t3) #store value in destiny tower
 addi $t3, $t3, 4
 
 finish:
-lw $ra, 0($sp)		#pop return address
-addi $sp, $sp, 4
+lw $ra, 0($sp)	#pop return address
+lw $t0, 4($sp)	#pop return N value
+addi $sp, $sp, 8
 jr $ra
 
 end:
